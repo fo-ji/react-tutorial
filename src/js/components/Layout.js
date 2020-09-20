361b93d1c47c4e394ee98f62a14482032d6b9a77
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-09-20 23:41:27 +0900

    Headerコンポーネントの実装

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,19 @@
 import React from "react";
+import Header from "./Header";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "fo-ji";
-  }
+  // constructor() {
+  //   super();
+  //   this.name = "fo-ji";
+  // }
   render() {
     return (
-    <h1>It's {this.name}!</h1>
+    // <h1>It's {this.name}!</h1>
+    <div>
+      <Header />
+      {/* <Header />
+      <Header /> */}
+    </div>
     );
   }
 }
