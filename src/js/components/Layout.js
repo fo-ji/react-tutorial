618f08ea218dd6d48c08355ca07ab4927e6cdece
fo-ji <57491651+fo-ji@users.noreply.github.com>
2020-09-21 12:56:42 +0900

    propsの実装サンプル

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,23 +3,27 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state = {name: "fo-ji"};
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {name: "fo-ji"};
+  // }
   render() {
-    let components = [<Header />, <Footer />];
+    // let components = [<Header />, <Footer />];
 
-    setTimeout(
-      () => { this.setState({name: "Hello"});}
-      , 1000
-    );
+    // setTimeout(
+    //   () => { this.setState({name: "Hello"});}
+    //   , 1000
+    // );
+    const title = "Welcome fo-ji!";
 
     return (
-    <div>
-      {this.state.name}
-      {components}
-    </div>
+      <div>
+        {/* {this.state.name}
+        {components} */}
+        <Header title={title} />
+        <Header title={"Thank you"} />
+        <Footer />
+      </div>
     );
   }
 }
